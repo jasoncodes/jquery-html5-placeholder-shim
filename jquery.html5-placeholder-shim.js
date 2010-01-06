53dda1fe5285df3377426eda46b15ba9d5b361ae
John Campbell <jcampbell1@gmail.com>
2010-01-06 01:13:35 -0500

    restructured the code

--- jquery.html5-placeholder-shim.js
+++ jquery.html5-placeholder-shim.js
@@ -1,53 +1,55 @@
 (function($) {
-	$.extend($,{
-		'shimPlaceholders': function() {
-			$(function(opts) {
+	$.extend($,{ placeholder: {
+			browser_supported: function() {
+				return this._supported !== undefined ?
+					this._supported :
+					( this._supported = !!('placeholder' in $('<input type="text">')[0]) );
+			},
+			shim: function(opts) {
 				var config = {
 					color: '#888',
 					cls: '',
 					lr_padding:4
-					
-				},
-				$i = $('input[placeholder]');
+				};
 				$.extend(config,opts);
-				if($i.length) {
-					var support_placeholder = !!('placeholder' in $('<input type="text">')[0])
-					if(!support_placeholder)
-						$i.each(function() {
-							
-							if( $(this).data('placeholder') )
-								return true;
-							
-							var ol = $('<label />')
-								.text($(this).attr('placeholder'))
-								.addClass(config.cls)
-								.css({ 
-									position:'absolute', 
-									overflow:'hidden', 
-									whiteSpace:'nowrap',
-									textAlign: 'left',
-									color: config.color, 
-									fontSize: parseInt($(this).height() * .85),
-									top: $(this).offset().top + ($(this).outerHeight() - $(this).height()) /2 + $(this).height()*.07,
-									left: $(this).offset().left + config.lr_padding,
-									width: $(this).width() - config.lr_padding  
-								})
-								.data('target',$(this))
-								.click(function(){
-									$(this).data('target').focus()
-								})
-								.appendTo('body')
-							$(this)
-								.data('placeholder',ol)
-								.focus(function(){
-									ol.hide();
-								}).blur(function() {
-									ol[$(this).val().length ? 'hide' : 'show']();
-								}).triggerHandler('blur');
+				!this.browser_supported() && $('input[placeholder]')._placeholder_shim(config);
+			}
+	}});
+
+	$.extend($.fn,{
+		_placeholder_shim: function(config) {
+			return this.each(function() {
+				if( $(this).data('placeholder') )
+					return true;
+				
+				var ol = $('<label />')
+					.text($(this).attr('placeholder'))
+					.addClass(config.cls)
+					.css({ 
+						position:'absolute', 
+						overflow:'hidden', 
+						whiteSpace:'nowrap',
+						textAlign: 'left',
+						color: config.color, 
+						fontSize: parseInt($(this).height() * .85),
+						top: $(this).offset().top + ($(this).outerHeight() - $(this).height()) /2 + $(this).height()*.07,
+						left: $(this).offset().left + config.lr_padding,
+						width: $(this).width() - config.lr_padding  
 					})
-				}
-			})
-		}
+					.data('target',$(this))
+					.click(function(){
+						$(this).data('target').focus()
+					})
+					.appendTo('body')
+				$(this)
+					.data('placeholder',ol)
+					.focus(function(){
+						ol.hide();
+					}).blur(function() {
+						ol[$(this).val().length ? 'hide' : 'show']();
+					}).triggerHandler('blur');
+			});
+		} 
 	});
 	
 })(jQuery);
